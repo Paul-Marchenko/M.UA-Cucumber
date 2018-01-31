2b3bc6430948400afbd245da5b19c24997ef97f3
Paul-Marchenko <pmarchenko@cogniance.com>
2018-01-31 16:09:53 +0200

    firstCommit

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -5,24 +5,27 @@ import cucumber.api.java.en.When;
 
 public class Login {
 
-@Given("^User is on Home Page$")
-    public void givenStatement(){
-    System.out.println("User is on Home Page");
+    @Given("^User is on Home Page$")
+    public void givenStatement() {
+        System.out.println("User is on Home Page");
 
-}
+    }
 
     @When("^User Navigate to LogIn Page$")
-    public void whenStatement(){
+    public void whenStatement() {
         System.out.println("User is on Login Page");
 
     }
+
     @And("^User enters UserName and Password$")
-    public void andStatement(){
-        System.out.println("Success entered");
+    public void andStatement() {
+        System.out.println("Success UserName");
+        System.out.println("Success Pass");
 
     }
+
     @Then("^Message displayed Login Successfully$")
-    public void thenStatement(){
+    public void thenStatement() {
         System.out.println("login success");
 
     }
